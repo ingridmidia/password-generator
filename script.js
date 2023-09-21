6dbd309f2ce4e746fb13ab2505265feffe6f3186
ingridmidia <ingridmidia@gmail.com>
2023-09-20 20:40:24 -0500

    organized initial logic

--- script.js
+++ script.js
@@ -18,48 +18,54 @@ var containsLowercase;
 var containsUppercase;
 var containsNumber;
 var containsSpecialCharacters;
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]","^", "_", "`", "{", "|", "}", "~"];
 
 function generatePassword() {
-  
+  getLength();
+  askLowercase();
+  askUppercase();
+  askNumber();
+  askSpecialCharacters();
+  validateInput();
+
+  console.log(passwordLength);
+  console.log(containsLowercase);
+  console.log(containsUppercase);
+  console.log(containsNumber);
+  console.log(containsSpecialCharacters);
 }
 
 function getLength() {
   passwordLength = prompt("Type the number of characters your password needs to have:");
 
-  if ((passwordLength >= 8) && (passwordLength <= 128)) {
-    askLowercase();
-  } else {
+  if ((passwordLength < 8) || (passwordLength > 128)) {
     alert("You password needs to have at least 8 characters and no more than 128 characters");
     getLength();
-  }
+  } 
 }
 
 function askLowercase() {
   containsLowercase = confirm("Do you want to include lowercase characters?");
-  askUppercase();
 }
 
 function askUppercase() {
   containsUppercase = confirm("Do you want to include uppercase characters?");
-  askNumber();
 }
 
 function askNumber() {
   containsNumber = confirm("Do you want to include numbers?");
-  askSpecialCharacters();
 }
 
 function askSpecialCharacters() {
   containsSpecialCharacters = confirm("Do you want to include special characters?");
-  validateInput();
 }
 
 function validateInput() {
-  if (containsLowercase || containsUppercase || containsNumber || containsSpecialCharacters) {
+  if (!containsLowercase && !containsUppercase && !containsNumber && !containsSpecialCharacters) {
+    alert("You must choose at least one type of characters");
     generatePassword();
-  } else {
-    alert("You must chose at least one type of characters");
-    askLowercase();
-  }
+  }  
 }
