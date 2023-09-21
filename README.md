f926004e8662a149806a0101422165cf128f4d88
ingridmidia <ingridmidia@gmail.com>
2023-09-21 17:24:47 -0500

    added comments and readme file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# password-generator
+# Password Generator
+
+## Description
+This page generates a strong random password based on user's criteria. User can pick how many characters they want the password to have and if those characters will be lowercase, uppercase, numbers, and/or special characters.
+
+![Alt text](./password-generator-screenshot.png)
+
+## [Deployed Website](https://ingridmidia.github.io/password-generator/)
--- script.js
+++ script.js
@@ -13,22 +13,25 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Declare variables for user input
 var passwordLength;
 var containsLowercase;
 var containsUppercase;
 var containsNumber;
 var containsSpecialCharacters;
 
+// Get lenght for user's password and stores information in variable passwordLength
 function getLength() {
   passwordLength = prompt("Type the number of characters your password needs to have:");
+  // Convert string to number in order to use variable in a for loop later in the code
   passwordLength = parseInt(passwordLength);
-
+  //Validates if user entered a length within 8 and 128 characters
   if ((passwordLength < 8) || (passwordLength > 128)) {
     alert("You password needs to have at least 8 characters and no more than 128 characters");
     getLength();
   }
 }
-
+// Get type of characters that user wants in their password and stores information in variables
 function getCharactersType() {
   containsLowercase = confirm("Do you want to include lowercase characters?");
   containsUppercase = confirm("Do you want to include uppercase characters?");
@@ -36,6 +39,7 @@ function getCharactersType() {
   containsSpecialCharacters = confirm("Do you want to include special characters?");
 }
 
+// Validate if user picked at least one type of character
 function validateInput() {
   if (!containsLowercase && !containsUppercase && !containsNumber && !containsSpecialCharacters) {
     alert("You must choose at least one type of characters");
@@ -43,17 +47,21 @@ function validateInput() {
   }
 }
 
+// Generate password based on conditions
 function generatePassword() {
+  // Arrays with all characters in each type
   var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+  // Array with all characters based on user's picks
   var possibleCharacters = [];
 
   getLength();
   getCharactersType();
   validateInput();
 
+  // Add characters that user picked on possibleCharacters array that was previously empty
   if (containsLowercase) {
     possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
   } if (containsUppercase) {
@@ -64,13 +72,14 @@ function generatePassword() {
     possibleCharacters = possibleCharacters.concat(specialCharacters);
   }
 
+  // Variable that will contain final password
   var password = "";
 
+  // Add random character to password string as many times as user picked as their password length
   for (var i = 0; i < passwordLength; i++) {
     password = password.concat(possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)]);
   }
 
-  validatePassword(password);
-
   return password;
 }
+
