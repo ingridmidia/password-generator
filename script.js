e5f5837bfc04b2538bc8905fc21213c115a1f461
ingridmidia <ingridmidia@gmail.com>
2023-09-21 10:01:01 -0500

    created possibleCharacters array and added logic to add type of characters selected by user

--- script.js
+++ script.js
@@ -18,10 +18,6 @@ var containsLowercase;
 var containsUppercase;
 var containsNumber;
 var containsSpecialCharacters;
-var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]","^", "_", "`", "{", "|", "}", "~"];
 
 function getLength() {
   passwordLength = prompt("Type the number of characters your password needs to have:");
@@ -29,7 +25,7 @@ function getLength() {
   if ((passwordLength < 8) || (passwordLength > 128)) {
     alert("You password needs to have at least 8 characters and no more than 128 characters");
     getLength();
-  } 
+  }
 }
 
 function getCharactersType() {
@@ -43,11 +39,27 @@ function validateInput() {
   if (!containsLowercase && !containsUppercase && !containsNumber && !containsSpecialCharacters) {
     alert("You must choose at least one type of characters");
     getCharactersType();
-  } 
+  }
 }
 
 function generatePassword() {
+  var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+  var possibleCharacters = [];
+
   getLength();
   getCharactersType();
   validateInput();
+
+  if (containsLowercase) {
+    possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
+  } if (containsUppercase) {
+    possibleCharacters = possibleCharacters.concat(uppercaseCharacters);
+  } if (containsNumber) {
+    possibleCharacters = possibleCharacters.concat(numberCharacters);
+  } if (containsSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+  }
 }
